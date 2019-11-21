ae919cc8daa6cc84c77da457f6b7cf03f05d55b0
anaezes <anaezes@gmail.com>
2019-11-21 16:55:12 +0000

    Change the ambient color according to data temperature.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var color = [];
+var index = 0;
+
 AFRAME.registerComponent('wobble-normal', {
 	schema: {},
 	tick: function (t) {
@@ -5,6 +8,12 @@ AFRAME.registerComponent('wobble-normal', {
 		this.el.components.material.material.normalMap.offset.y += 0.0001 * Math.cos(t/8000);
 		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.cos(t/1000);
 		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.sin(t/1200);
+
+		if(index < color.length){
+			console.log("color: " + color[index]);	
+			this.el.setAttribute('material', 'color', color[index]);
+			index++;
+		}
 	}
 })
 
@@ -28,4 +37,26 @@ AFRAME.registerPrimitive('a-ocean-plane', {
 		},
 		'wobble-normal': {}
 	},
+});
+
+
+
+d3.csv("data/Temperature.csv", function(data) {
+	data.forEach(function(d) {
+		d.time = d["timestamp (seconds since 01/01/1970)"];
+	    d.system = d[" system"];
+		d.entity = d[" entity "] ;
+		d.value = d[" value (°c)"];
+	});
+	data.forEach(function(d) {
+		if(d.entity === " Depth Sensor") {
+			console.log("value: " + d.value);
+			if(d.value < 10.3)
+				color.push('#8ab3b3');
+			else if(d.value > 10.3 & d.value < 10.4)
+				color.push('#04B431');
+			else
+				color.push('#FF0000');
+		}
+	});
 });
