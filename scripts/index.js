e7c5347bdfa957e028825e4ce2d093383bf83863
anaezes <anaezes@gmail.com>
2019-11-22 18:36:43 +0000

    Added dat.Gui to water temperature.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,32 @@
-var color = [];
-var index = 0;
+let color = [];
+let values = [];
+let index = 0;
+var fizzyText; 
 
+window.onload = function() {
+	var FizzyText = function() {
+		this.value = values[index];
+		this.color = color[index];
+		this.show = false;
+	};
+
+	fizzyText = new FizzyText();
+	var gui = new dat.GUI();
+	gui.add(fizzyText, 'value', -2, 30).listen();
+	gui.addColor(fizzyText, 'color').listen();
+	gui.add(fizzyText, 'show');
+
+	var update = function() {
+		requestAnimationFrame(update);
+		fizzyText.value =values[index];
+		fizzyText.color =color[index];
+	  };
+	  
+	update();
+
+}
+
+  
 AFRAME.registerComponent('wobble-normal', {
 	schema: {},
 	tick: function (t) {
@@ -9,8 +35,7 @@ AFRAME.registerComponent('wobble-normal', {
 		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.cos(t/1000);
 		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.sin(t/1200);
 
-		if(index < color.length){
-			console.log("color: " + color[index]);	
+		if(fizzyText.show && index < color.length ){
 			this.el.setAttribute('material', 'color', color[index]);
 			index++;
 		}
@@ -50,7 +75,7 @@ d3.csv("data/Temperature.csv", function(data) {
 	});
 	data.forEach(function(d) {
 		if(d.entity === " Depth Sensor") {
-			console.log("value: " + d.value);
+			values.push(d.value);
 			if(d.value < 10.3)
 				color.push('#8ab3b3');
 			else if(d.value > 10.3 & d.value < 10.4)
