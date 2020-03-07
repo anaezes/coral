50056bb49f1f55090fc92f65d1bb3e7c06b0af44
anaezes <anaezes@gmail.com>
2020-03-07 19:53:46 +0000

    Firsts steps to integrate three and cesium

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
+
+import * as THREE from "three";
+
+
+class App extends Component {
+   componentDidMount() {
+   // Cesium code
+   this.CesiumViewer = new Viewer(this.CesiumContainer);
+
+   var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    //document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    };
+    animate();
+    this.mount.appendChild(renderer.domElement);
+}
+
+ render() {
+        return (
+           <div>
+        <div id="CesiumContainer" ref={ element => this.CesiumContainer = element }/>
+
+        <div
+          id="ThreeContainer"
+          style={{ width: "80vw", height: "40vw" }}
+          ref={mount => {
+            this.mount = mount;
+          }}
+        />
+     </div>
+        );
+    }
+}
+    
+
+export default App;
